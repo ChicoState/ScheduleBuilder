ec8ed09371042a63687bfd1a6d9031d7f4fe19b5
tesss24 <tessshelton24@gmail.com>
2023-10-18 20:19:18 -0700

    More changes to form

--- ScheduleBuilder/ScheduleBuilder/settings.py
+++ ScheduleBuilder/ScheduleBuilder/settings.py
@@ -46,7 +46,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'core',
-    'google_calendar'
+    'google_calendar',
+    'social_django'
 ]
 
 MIDDLEWARE = [
